f167f5b739823bf80422c6bebf93c53f2a476ad1
alxmkh <walx404@gmail.com>
2025-09-09 12:04:27 +0300

    add funcs

--- main.go
+++ main.go
@@ -6,6 +6,16 @@ func main() {
 	const USD = 30
 	const EUR = 40
 	const RUB = 20
-	// Output
 	fmt.Println(EUR * RUB)
+}
+
+func convert(a float64, b, c string) {
+
+}
+
+func userInput() (float64, string, string) {
+	var a float64
+	var b, c string
+	fmt.Scan(&a, &b, &c)
+	return a, b, c
 }
