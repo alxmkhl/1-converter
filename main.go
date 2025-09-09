3358c9558a850ad314ec8436e7cdc99c2dab09bb
alxmkh <walx404@gmail.com>
2025-09-09 17:54:28 +0300

    task-3

--- main.go
+++ main.go
@@ -1,21 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+const USD = 30
+const EUR = 40
+const RUB = 1
 
 func main() {
-	const USD = 30
-	const EUR = 40
-	const RUB = 20
-	fmt.Println(EUR * RUB)
+
+	for {
+		howMuch, from, to, err := userInput()
+		if err != nil {
+			break
+		}
+		fmt.Println(howMuch, from, to)
+
+	}
 }
 
-func convert(a float64, b, c string) {
+func convert(howMuch float64, from, to string) {
+	switch from {
+	case "EUR":
 
+	}
 }
 
-func userInput() (float64, string, string) {
+func userInput() (float64, string, string, error) {
+	fmt.Println("Введите количество валюты  и ее тип (USD/UER)")
 	var a float64
 	var b, c string
-	fmt.Scan(&a, &b, &c)
-	return a, b, c
+	_, err := fmt.Scan(&a, &b, &c)
+	if err != nil {
+		return _, _, _, errors.New("Ошибка ввода")
+	}
+	return a, b, c, _
 }
